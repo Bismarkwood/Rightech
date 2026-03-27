18af142f77f0ec66aa6937bd1862de67bdcda17c
Bismarkwood <126250596+Bismarkwood@users.noreply.github.com>
2026-03-27 14:56:09 +0000

    fix: layout scroll

--- src/app/components/DashboardLayout.tsx
+++ src/app/components/DashboardLayout.tsx
@@ -145,7 +145,7 @@ export default function DashboardLayout() {
                               <Sidebar />
                               <div className="flex-1 flex flex-col min-w-0 h-full relative">
                                 <Topbar />
-                                <main className="flex-1 overflow-hidden">
+                                <main className="flex-1 h-full flex flex-col overflow-hidden">
                                   <Outlet />
                                 </main>
                                 
